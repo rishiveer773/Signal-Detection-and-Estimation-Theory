ac0f67fc5f632bf37467f98c3fac35fc0bc8212c
JackSparrow9875 <rishiveer07@outlook.com>
2024-09-01 15:40:56 +0530

    Test changes made

--- Assignment_2.m
+++ Assignment_2.m
@@ -1,6 +1,11 @@
+clear all
+close all
+clc
+
+
 % Defining the vectors for N and phi
-N_values = [10, 20];
-phi_values = [pi/8, pi/4];
+N_values = [50, 100];
+phi_values = [0, 45];
 
 % Defining the other required parameteres
 A = 1;                                
@@ -9,10 +14,10 @@ awgn_variance = 1;
 
 
 % Frequency range
-frequencies = -.2:.01:.2;
+frequencies = -1:.01:1;
 
 % Preallocating the matrix for CRLB values
-crlb_values = zeros(length(frequencies), length(N_values), length(phi_values));
+crlb_values = zeros(length(N_values), length(phi_values), length(frequencies));
 
 for i = 1:length(N_values)
     N = N_values(i);
@@ -22,21 +27,21 @@ for i = 1:length(N_values)
             f0 = frequencies(k);
             sum_term = 0;
             for n = 0:N-1
-                sum_term = sum_term + (2*pi*n * sin(2*pi*f0*n + phi))^2;
+                sum_term = sum_term + (2*pi*n * sind(2*pi*f0*n + phi))^2;
             end
             crlb = awgn_variance / (A^2 * sum_term);
-            crlb_values(k, i, j) = crlb;
+            crlb_values(i, j, k) = crlb;
         end
     end
 end
 
-% Plotting the CRLB vs frequency graph for different N and phi
+%Plotting the CRLB vs frequency graph for different N and phi
 figure;
 colors = ['r', 'g', 'b', 'k'];
 for i = 1:length(N_values)
     for j = 1:length(phi_values)
         subplot(length(N_values), length(phi_values), (i-1)*length(phi_values) + j);
-        plot(frequencies, crlb_values(:, i, j), 'Color', colors((i-1)*length(phi_values) + j), ...
+        plot(frequencies, reshape(crlb_values(i, j, :),1,201), 'Color', colors((i-1)*length(phi_values) + j), ...
             'LineWidth', 2);
         xlabel('Frequency (Hz)');
         ylabel('Cramer-Rao Lower Bound (CRLB)');
@@ -44,3 +49,6 @@ for i = 1:length(N_values)
         grid on;
     end
 end
+
+
+% plot(frequencies,crlb_values(1,:))
